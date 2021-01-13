5095a83cc913d9f07711424c1cc9b9e8dfb41ee6
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-13 11:36:46 +0530

    first commit

---
+++ src/classAndObject/Student.java
@@ -0,0 +1,16 @@
+package classAndObject;
+
+public class Student {
+	
+	//variable declaration
+	int id;
+	String name;
+	String gender;
+	
+	//method definitions
+	boolean updateProfile(String newName) {
+		name = newName;
+		return true;
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author bhavi
+ *
+ */
+module JAVAProject1 {
+}
