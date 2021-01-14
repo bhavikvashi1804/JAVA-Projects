16787d543d61db8a628bc718a18ea885ef34102d
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-14 11:51:47 +0530

    Arithmetic Operators

---
+++ src/classAndObject/ArithmeticOperator.java
@@ -0,0 +1,13 @@
+package classAndObject;
+
+public class ArithmeticOperator {
+	
+	public static void main(String[] args) {
+		System.out.println("5 + 2 = "+ (5+2));
+		System.out.println("5 - 2 = "+ (5-2));
+		System.out.println("5 * 2 = "+ (5*2));
+		System.out.println("5 / 2 = "+ (5/2));
+		System.out.println("5 % 2 = "+ (5%2));
+	}
+
+}
