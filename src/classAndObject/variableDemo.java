757d97d9779fcbe9e57006350f679731fbef01da
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-14 11:03:03 +0530

    Variable Demo

---
+++ src/classAndObject/variableDemo.java
@@ -0,0 +1,20 @@
+package classAndObject;
+
+public class variableDemo {
+
+	// all below variable are allowed
+	int id= 0;
+	int Id = 0;
+	int iD = 0;
+	int ID = 0;
+	
+	// class,variable, method names
+	/*
+	 	Duplicate names are not allowed
+	 	Name must start with _ / $ / letter
+	 	Name does not start with number
+	 	No reserved keyword used for name
+	*/
+	
+	//Every statement ends with ;
+}
