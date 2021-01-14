d7c8636ecf0c6fad3770ff24cccd28c5a8c5c452
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-14 11:11:53 +0530

    Print on console

---
+++ src/classAndObject/PrintOnConsole.java
@@ -0,0 +1,25 @@
+package classAndObject;
+
+public class PrintOnConsole {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello");
+		System.out.print(" ");
+		System.out.print("How are you?");
+		//print only prints a line
+		
+		
+		System.out.println("Hello");
+		System.out.println("");
+		System.out.println("How are you?");
+		//println prints a line + sets cursor to new line
+		
+		
+		System.out.println("--------------------");
+		System.out.println("Hello\tHow are you?");
+		System.out.println("Line 1\nLine 2");
+		// \t: new tab
+		// \n: new line
+	}
+}
