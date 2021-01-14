499ccad7b5b1979267ba5cdbb2d745716a17c27d
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-14 10:59:29 +0530

    Comments

---
+++ src/classAndObject/CommentsDemo.java
@@ -0,0 +1,18 @@
+package classAndObject;
+
+public class CommentsDemo {
+
+	public static void SingleLineComment() {
+		// this is single line comment
+	}
+	
+	public static void MultipleLineComment() {
+		/*
+			This is multi-line comment
+			Line1
+			Line2
+			Line3
+		 
+		*/
+	}
+}
