2e8766fa057567061e3cf32259c72b357830b70d
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-19 12:40:27 +0530

    Ternary Operator

---
+++ src/operatorsDemo/TernaryOperator.java
@@ -0,0 +1,16 @@
+package operatorsDemo;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 20;
+		int y = 30;
+		
+		String result  = (x>y)?"X is greater":"Y is greater";
+		System.out.println(result);
+
+	}
+
+}
