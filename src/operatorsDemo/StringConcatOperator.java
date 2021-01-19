90ca02391679f6d9c764a0751c871d9a977d9be0
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-19 10:26:43 +0530

    String Concat "+"

---
+++ src/operatorsDemo/StringConcatOperator.java
@@ -0,0 +1,13 @@
+package operatorsDemo;
+
+public class StringConcatOperator {
+	
+	public static void main(String[] args) {
+		String firstName = "Bhavik";
+		String lastName = "Vashi";
+		int age = 24;
+		
+		System.out.println("First Name: "+firstName +"\nLast Name: "+lastName+"\nAge: "+age);
+	}
+
+}
