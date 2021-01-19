49dafdb845fa455f06129c62e3c8b46357985821
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-19 09:58:38 +0530

    Arithmetic Operators

--- src/operatorsDemo/ArithmeticOperators.java
+++ src/operatorsDemo/ArithmeticOperators.java
@@ -1,13 +1,15 @@
-package classAndObject;
+package operatorsDemo;
 
-public class ArithmeticOperator {
-	
+public class ArithmeticOperators {
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		System.out.println("5 + 2 = "+ (5+2));
 		System.out.println("5 - 2 = "+ (5-2));
 		System.out.println("5 * 2 = "+ (5*2));
 		System.out.println("5 / 2 = "+ (5/2));
 		System.out.println("5 % 2 = "+ (5%2));
+
 	}
 
 }
