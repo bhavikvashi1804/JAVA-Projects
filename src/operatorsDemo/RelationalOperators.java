2d7f4dc36a81cecf623ac7e9c3129f6310f61847
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-19 11:56:04 +0530

    RelationalOperators

---
+++ src/operatorsDemo/RelationalOperators.java
@@ -0,0 +1,15 @@
+package operatorsDemo;
+
+public class RelationalOperators {
+	
+	 public static void main(String[] args) {
+		 
+		 System.out.println(10<=20);
+		 System.out.println('a'<10);
+		 System.out.println('a'>'A');
+		 System.out.println('a'>900);
+		 System.out.println((int)'a'); //97
+		 System.out.println((int)'A'); //65
+		 
+	 }
+}
