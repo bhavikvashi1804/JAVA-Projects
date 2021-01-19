0ae75348e9cad90c24745ca888f73b4e367026de
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-19 09:23:11 +0530

    Explicit Type casting beyond the range

--- src/dataTypesInJava/ExplicitTypeCast.java
+++ src/dataTypesInJava/ExplicitTypeCast.java
@@ -21,5 +21,26 @@ public class ExplicitTypeCast {
 		byte result = (byte)(a+h);
 		System.out.println(result);
 		//-30
+		
+		
+		//explicit type casting beyond the range
+		int y = 130;
+		byte b1 = (byte)y;
+		System.out.println(b1);
+		//value will be cal using the circle
+		// 127 +ve then -128, -127, -126
+		//so answer is -126
+		
+		
+		int m = 612;
+		//byte total number: 256: -128 to 127
+		// rounds = 612/ 256 = 2.39 = 2
+		// left numbers =  612 - (256*2) = 612-512 = 100
+		byte n = (byte)m;
+		//100
+		char ch1 = (char)n;
+		System.out.println(ch1);
+		//d
+		
 	}
 }
