ad2c64abb4fb55a8a704fd98fa4aa95aabb9c6aa
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-20 09:51:44 +0530

    For Loop

---
+++ src/FlowControlStatements/ForLoopDemo.java
@@ -0,0 +1,16 @@
+package FlowControlStatements;
+
+public class ForLoopDemo {
+	
+	public static void main(String[] args) {
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+		
+		
+		for(int i=5;i>=0;i--) {
+			System.out.println(i);
+		}
+	}
+
+}
