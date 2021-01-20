3764ad83f403e6a41d4e3ccad256ea0d80eb1bf7
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-20 11:29:19 +0530

    Create reference in static method

--- src/NonStaticMember/ThisDemo.java
+++ src/NonStaticMember/ThisDemo.java
@@ -2,7 +2,14 @@ package NonStaticMember;
 
 public class ThisDemo {
 
+	static Demo obj = new Demo();
+	
 	int x;
+	
+	static {
+		System.out.println(ThisDemo.obj);
+		ThisDemo.Init();
+	}
 	
 	ThisDemo(){
 		System.out.println(this);
@@ -12,5 +19,10 @@ public class ThisDemo {
 	public static void main(String[] args) {
 		ThisDemo d1 = new ThisDemo();
 		ThisDemo d2 = new ThisDemo();
+	}
+	
+	
+	public static void Init() {
+		ThisDemo.obj = new Demo();
 	}
 }
