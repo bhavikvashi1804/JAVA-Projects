ab2aeaef93dc42575a2d672863c9a1554169fee0
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-20 13:09:06 +0530

    StringBuffer and its method

---
+++ src/StringClassDemo/StringBufferDemo.java
@@ -0,0 +1,29 @@
+package StringClassDemo;
+
+public class StringBufferDemo {
+	public static void main(String[] args) {
+	
+		//mutable
+		
+		StringBuffer sb1 = new StringBuffer();
+		System.out.println(sb1.capacity());
+		
+		sb1.append("All the power is with in you.");
+		sb1.append("You can do anything and everything.");
+		
+		System.out.println(sb1);
+		System.out.println(sb1.capacity());
+		
+		System.out.println(sb1.charAt(10)); //w
+		
+		StringBuffer sb2 = new StringBuffer("abcde");
+		System.out.println(sb2.reverse()); //edcba
+		
+		
+		System.out.println(sb1.insert(10, "XYZ"));
+		//All the poXYZwer is with in you.You can do anything and everything.
+		
+		System.out.println(sb1.delete(10, 13));
+		//All the power is with in you.You can do anything and everything.
+	}
+}
