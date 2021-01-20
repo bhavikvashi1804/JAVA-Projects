aaf0b2c89e2ab94bf73e243e5929f9d90938510a
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-20 12:18:02 +0530

    String Pooling demo

---
+++ src/StringClassDemo/StringPoolDemo.java
@@ -0,0 +1,33 @@
+package StringClassDemo;
+
+public class StringPoolDemo {
+	
+	
+	public static void main(String[] args) {
+		User user1 = new User(1,"Bhavik");
+		User user2 = new User(2,"Bhavik");
+		
+		System.out.println(user1);
+		System.out.println(user2);
+		
+		
+		String s1="Raj";
+		String s2="Raj";
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+		
+		
+	}
+
+}
+
+
+class User{
+	int id;
+	String name;
+	
+	User(int id,String name){
+		this.id = id;
+		this.name = name;
+	}
+}
