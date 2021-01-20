940c2bc92d2c58cf40d1133cac726678f9f95747
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-20 09:45:42 +0530

    Do while

---
+++ src/FlowControlStatements/DoWhileDemo.java
@@ -0,0 +1,19 @@
+package FlowControlStatements;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x = 1;
+		
+		do {
+			System.out.println("Inside Loop: "+x);
+			x++;
+		}
+		while(x<11);
+
+	}
+
+}
