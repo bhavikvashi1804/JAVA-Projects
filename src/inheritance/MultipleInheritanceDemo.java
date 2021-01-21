ef86d5e02807a3c72607a6b2f2f952dd3a20206e
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-21 15:49:29 +0530

    Final Demo

---
+++ src/inheritance/MultipleInheritanceDemo.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+
+interface AnimalEat{
+    void eat();
+}
+
+interface  AnimalTravel{
+    void travel();
+}
+
+class MyAnimal implements  AnimalEat,AnimalTravel{
+
+    @Override
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    @Override
+    public void travel() {
+        System.out.println("Animal is travellings");
+    }
+}
+
+public class MultipleInheritanceDemo {
+    public static void main(String[] args) {
+        MyAnimal animal = new MyAnimal();
+        animal.eat();
+        animal.travel();
+    }
+}
