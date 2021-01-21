94fb4060219e37aa06ba9c3472acda05b9dd0a0e
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-21 13:33:03 +0530

    Super Keyword and constructor

---
+++ src/superdemo/SuperWithConstructor.java
@@ -0,0 +1,18 @@
+package superdemo;
+
+public class SuperWithConstructor {
+    public static void main(String[] args) {
+        Dog2 d=new Dog2();
+    }
+}
+
+class Animal2{
+    Animal2(){System.out.println("animal is created");}
+}
+class Dog2 extends Animal2{
+    Dog2(){
+        super();
+        System.out.println("dog is created");
+    }
+}
+
