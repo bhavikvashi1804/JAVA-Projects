73f70208e04d36814e80be8c065c8176413e4ab9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-21 13:30:56 +0530

    Super Keyword and method

---
+++ src/superdemo/SuperWithMethod.java
@@ -0,0 +1,20 @@
+package superdemo;
+
+public class SuperWithMethod {
+    public static void main(String[] args) {
+        Dog1 d=new Dog1();
+        d.work();
+    }
+}
+
+class Animal1{
+    void eat(){System.out.println("eating...");}
+}
+class Dog1 extends Animal1{
+    void eat(){System.out.println("eating bread...");}
+    void bark(){System.out.println("barking...");}
+    void work(){
+        super.eat();
+        bark();
+    }
+}
