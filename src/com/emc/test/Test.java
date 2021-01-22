f77978a5532cf1ed59495c0a98945732929dc3e2
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-22 13:33:47 +0530

    Effect of encapsulation in main class

--- src/com/emc/test/Test.java
+++ src/com/emc/test/Test.java
@@ -7,24 +7,20 @@ public class Test {
 
     public static void main(String[] args) {
         Organizer organizer = new Organizer();
-        organizer.id = 123l;
-        organizer.name = "Apple Inc";
-        System.out.println(organizer.id);
-        System.out.println(organizer.name);
+
 
 
         Event event = new Event();
-        event.id = 456l;
-        event.name = "iPhone 13 Launch";
-        event.description = "Grand Launch";
-        System.out.println(event.id);
-        System.out.println(event.name);
-        System.out.println(event.description);
+        event.setId(456l);
+        event.setName("iPhone 13 Launch");
+        event.setDescription("Grand Launch");
 
-        Event event1 = new Event(789l,"iPhone 14 Launch","Grand Launch");
-        System.out.println(event1.id);
-        System.out.println(event1.name);
-        System.out.println(event1.description);
+
+        System.out.println(event.getId());
+        System.out.println(event.getName());
+        System.out.println(event.getDescription());
+
+
 
 
     }
