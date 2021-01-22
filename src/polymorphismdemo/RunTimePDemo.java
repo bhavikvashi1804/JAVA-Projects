a31979046136ba21fae7b339736c471c682d509e
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-22 09:09:24 +0530

    Run Time Polymorphism Demo

---
+++ src/polymorphismdemo/RunTimePDemo.java
@@ -0,0 +1,15 @@
+package polymorphismdemo;
+
+public class RunTimePDemo {
+    public static void main(String[] args) {
+        Bike b = new Splendor();//upcasting
+        b.run();
+    }
+}
+
+class Bike{
+    void run(){System.out.println("running");}
+}
+class Splendor extends Bike{
+    void run(){System.out.println("running safely with 60km");}
+}
