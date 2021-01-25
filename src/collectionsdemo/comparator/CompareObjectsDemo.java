3699e5190e501d8682b795058d23333ce8a2ce7a
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 12:39:29 +0530

    Create an own Name Comparator for Student Class

--- src/collectionsdemo/comparator/CompareObjectsDemo.java
+++ src/collectionsdemo/comparator/CompareObjectsDemo.java
@@ -1,19 +1,15 @@
 package collectionsdemo.comparator;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class CompareObjectsDemo {
     public static void main(String[] args) {
-        Student s1 = new Student("Bhavik",22);
 
-        Student s2 = new Student("Raj",22);
-        Student s3 = new Student("Bhavik",22);
 
-        System.out.println(s1.compareTo(s2));
-        System.out.println(s1.compareTo(s3));
-
-        Set<Student> set = new HashSet<Student>();
+        Set<Student> set = new TreeSet<Student>(new StudentNameComparator());
         set.add(new Student("Bhavik",22));
         set.add(new Student("Raj",21));
         set.add(new Student("Yash",22));
@@ -57,5 +53,15 @@ class Student implements Comparable<Student>{
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+}
+
+class  StudentNameComparator implements Comparator<Student> {
+
+    @Override
+    public int compare(Student o1, Student o2) {
+        String s1 = o1.name;
+        String s2 = o2.name;
+        return s1.compareTo(s2);
     }
 }
