c9bc44cc3094e072fe83f9f40b761830de10d747
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 13:32:03 +0530

    WeakHashMap Demo

---
+++ src/collectionsdemo/map/WeakHashMapDemo.java
@@ -0,0 +1,32 @@
+package collectionsdemo.map;
+
+import java.util.WeakHashMap;
+import java.util.Map;
+
+public class WeakHashMapDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Map<User,String> hashMap = new WeakHashMap<User,String>();
+
+        User u = new User();
+        hashMap.put(u,"Tom");
+        System.out.println(hashMap);
+
+        u = null;
+        System.gc();
+        Thread.sleep(5000);
+
+        System.out.println(hashMap);
+    }
+}
+class  User{
+    @Override
+    public String toString() {
+        return "user";
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Finalize called");
+    }
+}
