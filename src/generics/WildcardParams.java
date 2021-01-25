5887d4fa2940c1fd755ac29f529da8755ed7b382
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 17:06:59 +0530

    Check the wild card of array list with thread

--- src/generics/WildcardParams.java
+++ src/generics/WildcardParams.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 
 public class WildcardParams {
 
-    public void myMethod(ArrayList<?> l){
+    public void myMethod(ArrayList<? extends Thread> l){
         l.add(null);
         //l.add(5);
         //if ArrayList<?> then null values are allowed
+    }
+
+
+}
+
+class  Test1{
+    public static void main(String[] args) {
+        WildcardParams wp = new WildcardParams();
+        wp.myMethod(new ArrayList<Thread>());
     }
 }
