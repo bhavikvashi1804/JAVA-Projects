539322fca858790d33cafe73c77a4332df268d00
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 17:11:47 +0530

    Wildcard super

--- src/generics/WildcardParams.java
+++ src/generics/WildcardParams.java
@@ -11,11 +11,27 @@ public class WildcardParams {
     }
 
 
+    public void myMethod1(ArrayList<?  super B> l){
+        //this list can accept wich is super class of B
+        l.add(null);
+        //l.add(5);
+        //if ArrayList<?> then null values are allowed
+    }
+}
+
+class A{
+
+}
+
+class  B extends  A{
+
 }
 
 class  Test1{
     public static void main(String[] args) {
         WildcardParams wp = new WildcardParams();
         wp.myMethod(new ArrayList<Thread>());
+
+        wp.myMethod1(new ArrayList<A>());
     }
 }
