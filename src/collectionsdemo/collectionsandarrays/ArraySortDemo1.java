156c2761e3fdbd2a1cae893f447812bf29cbb7b9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 16:03:57 +0530

    Arrays.sort(a) with own Comparator

--- src/collectionsdemo/collectionsandarrays/ArraySortDemo1.java
+++ src/collectionsdemo/collectionsandarrays/ArraySortDemo1.java
@@ -12,5 +12,13 @@ public class ArraySortDemo1 {
         for(String x: array1){
             System.out.print(x+ " ");
         }
+
+
+        String[] array2 = {"Bhavik","Atul","Yash","Apple","Raj"};
+        Arrays.sort(array2, new MyComparator());
+        System.out.println("\nUsing own Comparator:");
+        for(String x: array2){
+            System.out.print(x+ " ");
+        }
     }
 }
