4f2a3c6ce9797e2bcb8de47f8d0b6155383bb161
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 09:09:03 +0530

    Restricting the data type of array list

--- src/collectionsdemo/list/ArrayListDemo.java
+++ src/collectionsdemo/list/ArrayListDemo.java
@@ -9,7 +9,15 @@ public class ArrayListDemo {
         al.add(20); // Autoboxing
         al.add(30.45);
         al.add("Hello");
-
         System.out.println(al);
+
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        arrayList.add(40);
+        System.out.println(arrayList);
+
     }
 }
