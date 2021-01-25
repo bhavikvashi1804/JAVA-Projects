b9b61430ab49c19c6a7ad36239aa40cf8820fa5a
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-25 13:05:35 +0530

    LinkedHashMap demo

---
+++ src/collectionsdemo/map/LinkedHashMapDemo.java
@@ -0,0 +1,26 @@
+package collectionsdemo.map;
+
+import java.util.*;
+
+public class LinkedHashMapDemo {
+
+    public static void main(String[] args) {
+        Map<String,Integer> map = new LinkedHashMap<>();
+
+        map.put("Bhavik",80);
+        map.put("Raj",81);
+        map.put("Yash",70);
+        map.put("Dhruvik",89);
+        map.put("Meet",80);
+
+        Set<String> keys = map.keySet();
+        System.out.println("Keys: "+ keys);
+
+        Collection<Integer> values = map.values();
+        System.out.println("Values: "+ values);
+
+        for(String key:keys){
+            System.out.println("Key: "+key+"\tValue: "+map.get(key));
+        }
+    }
+}
