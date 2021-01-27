8d805a0c7fb230f10e1905a04e691de04890bae5
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 09:21:26 +0530

    @SuppressWarnings

--- src/annotationdemo/B.java
+++ src/annotationdemo/B.java
@@ -1,12 +1,32 @@
 package annotationdemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class B {
+    @SuppressWarnings(value = {"deprecation"})
     public static void main(String[] args) {
         A a= new A();
         System.out.println(a);
 
         C c = new C();
         c.method1();
+
         c.method2();
+    }
+
+    @SuppressWarnings(value = {"deprecation","rawtypes"})
+    public static void myMethod(){
+        A a= new A();
+        System.out.println(a);
+
+        C c = new C();
+        c.method1();
+
+        c.method2();
+
+        List list = new ArrayList();
+        System.out.println(list);
     }
 }
