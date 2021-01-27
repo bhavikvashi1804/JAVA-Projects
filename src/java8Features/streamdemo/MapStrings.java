49cc7ac137cd54e4e6acc3d07a2bf24472a20206
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 22:49:20 +0530

    Map the String

---
+++ src/java8Features/streamdemo/MapStrings.java
@@ -0,0 +1,20 @@
+package java8Features.streamdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+public class MapStrings {
+    public static void main(String[] args) {
+        List<String> list= new ArrayList<>();
+        list.add("Bhavik");
+        list.add("RaJ");
+        list.add("AmaZon");
+
+        List<String> listInLowerCase = list.stream().map(s->s.toLowerCase()).collect(Collectors.toList());
+        for(String s: listInLowerCase){
+            System.out.println(s);
+        }
+    }
+}
