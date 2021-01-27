b29cb6714a1ec5dd069b095dd8a40be4cb6bcaa2
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 20:33:43 +0530

    Lambda Expression & Anonymous Class : new with Lambda Expression

--- src/java8Features/lambdasbasics/MyThread2.java
+++ src/java8Features/lambdasbasics/MyThread2.java
@@ -3,19 +3,17 @@ package java8Features.lambdasbasics;
 public class MyThread2 {
     public static void main(String[] args) {
 
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i=0;i<10;i++){
-                    System.out.println("Child method");
-                }
+        Thread t = new Thread(()-> {
+            for(int i=0;i<10;i++) {
+                System.out.println("Child method");
             }
         });
+
         t.start();
 
         for(int i=0;i<10;i++){
             System.out.println("Main method");
         }
-        //this is old code
+
     }
 }
