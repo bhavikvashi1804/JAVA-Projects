1e87d683d53a39c3d4b59cc93826678bd56f9d68
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 21:55:50 +0530

    Predicate join + pass method argument predicate & list of testing element :: join using=> negate(), add(), or()

--- src/java8Features/predicate/PredicateJoin.java
+++ src/java8Features/predicate/PredicateJoin.java
@@ -7,17 +7,25 @@ public class PredicateJoin {
         int[] x = {0,10,20,30,40,50,60,70,80,90};
 
         Predicate<Integer> predicate = (i)->(i>50);
+        Predicate<Integer> even = (i)->(i%2==0);
 
+        System.out.println(">50");
         check(predicate,x);
+        System.out.println("<=50");
+        check(predicate.negate(),x);
+
+        check(even,x);
+
+        System.out.println(">50 & Even");
+        check(predicate.and(even),x);
+
+
     }
 
     static void check(Predicate<Integer> p,int[] x){
         for (int i=0;i<x.length;i++){
-            if(p.test(x[i])){
-                System.out.println(x[i]+" > 50");
-            }
-            else{
-                System.out.println(x[i]+" <= 50");
+            if(p.test(x[i])) {
+                System.out.println(x[i]);
             }
         }
     }
