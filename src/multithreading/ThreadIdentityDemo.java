32a83c3e0ebd39fdf76d717025bd40ed9d056fd0
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 10:45:43 +0530

    Thread Priority

--- src/multithreading/ThreadIdentityDemo.java
+++ src/multithreading/ThreadIdentityDemo.java
@@ -11,6 +11,23 @@ public class ThreadIdentityDemo {
         t2.join();
 
         System.out.println("Current Thread Name: "+ Thread.currentThread().getName());
+
+
+        System.out.println("Thread Priority");
+        threadPriorityDemo();
+    }
+
+
+    public static void threadPriorityDemo(){
+        MyThread2 thread1 = new MyThread2();
+        thread1.setName("Low priority thread");
+        thread1.setPriority(Thread.MIN_PRIORITY);
+        thread1.start();
+
+        MyThread2 thread2 = new MyThread2();
+        thread2.setName("High priority thread");
+        thread2.setPriority(Thread.MAX_PRIORITY);
+        thread2.start();
     }
 }
 
@@ -20,6 +37,18 @@ class MyThread1 extends Thread{
         try {
             //to change the Thread Name
             currentThread().setName("MyThread");
+            System.out.println("Current Thread Name: "+ Thread.currentThread().getName());
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class  MyThread2 extends  Thread{
+    @Override
+    public void run() {
+        try {
             System.out.println("Current Thread Name: "+ Thread.currentThread().getName());
             Thread.sleep(1000);
         } catch (InterruptedException e) {
