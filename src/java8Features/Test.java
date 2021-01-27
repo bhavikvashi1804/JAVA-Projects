227bfc9dc4f52f5cd5431243d13bac0a284ccd10
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 20:02:51 +0530

    Call the Interface overridden method normally : upcasting

--- src/java8Features/Test.java
+++ src/java8Features/Test.java
@@ -2,7 +2,7 @@ package java8Features;
 
 public class Test {
     public static void main(String[] args) {
-        C c= new C();
-        c.myMethod();
+        A a= new C();
+        a.myMethod();
     }
 }
