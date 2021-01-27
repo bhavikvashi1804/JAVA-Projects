db59875ce850b231582df95c1a2965580f9f26b1
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 08:37:17 +0530

    De Serialization Demo

---
+++ src/serializationdemo/DeSerializationDemo.java
@@ -0,0 +1,27 @@
+package serializationdemo;
+
+import java.io.*;
+
+public class DeSerializationDemo {
+    public static void main(String[] args) {
+
+        FileInputStream fis;
+        ObjectInputStream ois;
+
+        try{
+            fis = new FileInputStream("d://empDetails.ser");
+            ois = new ObjectInputStream(fis);
+
+            Employee e = (Employee) ois.readObject();
+
+            System.out.println(e.id);
+            System.out.println(e.name);
+            System.out.println(e.salary);
+            System.out.println(e.ssn);
+
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
