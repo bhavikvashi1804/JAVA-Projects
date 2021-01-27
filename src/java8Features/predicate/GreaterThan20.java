bb69b1a170de2f92e33ce53a8a3b7e5e5f516d6b
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 21:34:00 +0530

    Predicate for Method

--- src/java8Features/predicate/GreaterThan20.java
+++ src/java8Features/predicate/GreaterThan20.java
@@ -8,5 +8,10 @@ public class GreaterThan20 {
         Predicate<Integer> predicate = (i)->(i>20);
         System.out.println(predicate.test(25));
         System.out.println(predicate.test(15));
+
+
+        Predicate<String> predicate1 = (s)->(s.length()>5);
+        System.out.println(predicate1.test("Bhavik"));
+        System.out.println(predicate1.test("Raj"));
     }
 }
