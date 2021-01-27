4e37d4fdb6f840681f95016a30c50753d3928f9a
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-27 23:50:27 +0530

    more methods

--- src/java8Features/streamdemo/FilterNumbers.java
+++ src/java8Features/streamdemo/FilterNumbers.java
@@ -2,6 +2,7 @@ package java8Features.streamdemo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -19,5 +20,35 @@ public class FilterNumbers {
             System.out.print(x+" ");
         }
         System.out.println();
+
+        double total = l1.stream().filter(i->i%2==0).count();
+        System.out.println("Total no of elements "+ total);
+
+
+
+
+
+        List<Integer> sorted = l1.stream().sorted().collect(Collectors.toList());
+
+        for(Integer x: sorted){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+
+        List<Integer> sorted1 = l1.stream().sorted((i1, i2)->i2.compareTo(i1)).collect(Collectors.toList());
+        for(Integer x: sorted1){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+
+
+        Integer min = l1.stream().min((i1, i2)->i1.compareTo(i2)).get();
+        System.out.println(min);
+        Integer max = l1.stream().max((i1, i2)->i1.compareTo(i2)).get();
+        System.out.println(max);
+
+
+        l1.stream().forEach((i)->System.out.println("Number is: "+ i));
     }
+
 }
