647ed29e9cc74f940fd79e01d473a228c8274ba2
Bhavik Vashi <bhavikvashi1804@gmail.com>
2021-01-28 16:23:19 +0530

    Stream Method demo

--- src/java8Features/streamdemo/StreamMethods.java
+++ src/java8Features/streamdemo/StreamMethods.java
@@ -3,12 +3,13 @@ package java8Features.streamdemo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamMethods {
 
     public static void main(String[] args) {
-        diff();
+        peekDemo();
     }
 
     public static  void forEachDemo(){
@@ -29,5 +30,14 @@ public class StreamMethods {
         Stream<String> vStream = vehicles.parallelStream();
         vehicles.add("bike");
         vStream.filter(str->str.length() > 3).map(String::toUpperCase).sorted().forEach(System.out::println);
+    }
+
+    public static void peekDemo(){
+        Stream.of("bus", "car", "bycle", "flight", "train")
+                .filter(e -> e.length() > 3)
+                .peek(e -> System.out.println("Filtered value: " + e))
+                .map(String::toUpperCase)
+                .peek(e -> System.out.println("Mapped value: " + e))
+                .collect(Collectors.toList());
     }
 }
